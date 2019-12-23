cb3f4a78fae8530c6485ac317c89ca05bf141135
Akshay Easwaran <akeaswaran@me.com>
2019-12-22 23:01:57 -0500

    tsp example

--- player_data.R
+++ player_data.R
@@ -123,7 +123,8 @@ generate_player_stat_chart <- function(player, team, stat1, stat1_title, stat2, 
     stats[[stat1]] <- factor(stats[[stat1]], levels = stats[[stat1]])
     base = ggplot(stats, aes_string(x=stat1, y=stat2, group=1))
 
-    base + geom_line(color=line_color) + geom_point(color=line_color) + ggtitle(paste0(player," (2019-20)",sep="")) + ylab(stat2_title) + xlab(stat1_title) + geom_hline(yintercept = mean(stats[[stat2]]), color="navy", linetype="dotted")
+    base + geom_line(color=line_color) + geom_point(color=line_color) + ggtitle(paste0(player," (2019-20)",sep="")) + ylab(stat2_title) + xlab(stat1_title) + geom_hline(yintercept = mean(stats[[stat2]]), color="navy", linetype="dashed")
 }
 
-generate_player_stat_chart("Michael Devoe", "Georgia Tech", "Opponent","Opponent", "eFG","Effective Field Goal %", "#B3A369")
+# example: Tracking Michael Devoe's TSP% so far in 2019-20
+generate_player_stat_chart("Michael Devoe", "Georgia Tech", "Opponent","Opponent", "TSP","True Shooting %", "#B3A369")
